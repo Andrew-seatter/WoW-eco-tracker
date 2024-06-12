6e751e9e71a3dc753397af0189b7a47025ec8f0d
squee72564 <rodriguez.al183@gmail.com>
2024-06-12 15:53:40 -0700

    Comment out datFormat import temporarily

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,6 @@
 const { Schema, model } = require("mongoose");
 const bcrypt = require("bcrypt");
-const dateFormat = require("../utils/dateFormat");
+//const dateFormat = require("../utils/dateFormat");
 const Saved = require('./saved.js');
 const PastSearches = require('./pastSearches.js');
 
